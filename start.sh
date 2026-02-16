30f24a41c8dfeef23fa63d169b4bb852e125b81f
bilberats <leroy.fernand@orange.fr>
2026-02-16 10:49:05 -0500

    Added start scipt, modified send dns_requests so that it can send requests with DoH, DoT and UDP,

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MONITORING_INTERVAL=${1:-1}      # seconds between samples
+DURATION=${2:-60}     # total duration
+QUERIES_INTERVAL=${1:-1}      # seconds between query count updates
+PROTOCOL=${4:-"udp"}  # protocols to monitor (space-separated)
+
+# Start all scripts from both servers
+ssh thd@157.159.55.165 "cd ~/dns-activity-monitoring && bash ./start_dns_logs_monitoring.sh $MONITORING_INTERVAL $DURATION" &
+SSH_PID=$!
+
+bash ./component-monitoring/start_measurements.sh $MONITORING_INTERVAL $DURATION &
+LOCAL_MEASUREMENTS_PID=$!
+
+python3 send_dns_requests.py $PROTOCOL 157.159.55.165 example.com $QUERIES_INTERVAL $DURATION &
+LOCAL_REQUESTS_PID=$!
+
+# Wait for both to finish
+wait $SSH_PID
+wait $LOCAL_MEASUREMENTS_PID
+wait $LOCAL_REQUESTS_PID
+
+echo "Measurements completed at $(date +%s.%N)"
